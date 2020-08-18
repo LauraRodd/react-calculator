59d2c3327bef0cb74d8e7fe3ca4c8fb00e615107
Laura Rodd <laura_rodd@hotmail.com>
2020-08-18 14:24:36 +0200

    Updates

---
+++ precache-manifest.31607bd661d955edb1ff82f10c21aaef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0901d273a81cfcf90eda20b95b80695",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "629af0322c6ad54c261e",
+    "url": "/react-calculator/static/css/main.dbce1bd4.chunk.css"
+  },
+  {
+    "revision": "7085b1248a8c46468acf",
+    "url": "/react-calculator/static/js/2.94ec6ec2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.94ec6ec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629af0322c6ad54c261e",
+    "url": "/react-calculator/static/js/main.5854a0f8.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  }
+]);
